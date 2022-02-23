3963986fa42f1bbb4b5fc98fa3bf65596f02b6bf
ShayanG9 <36546186+ShayanG9@users.noreply.github.com>
2022-02-23 14:19:56 -0500

    Project file

---
+++ RaceCar/RaceCar/main.c
@@ -0,0 +1,18 @@
+/*
+ * RaceCar.c
+ *
+ * Created: 2/23/2022 2:19:17 PM
+ * Author : ShayanG_
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
